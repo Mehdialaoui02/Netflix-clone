5fdcfed241f85f2595ab55b27adc7b3ed1452ff0
Mehdialaoui02 <mehdi2002alaoui@gmail.com>
2024-08-25 20:04:45 +0200

    SignUp and Login full functionnalities

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useAuthStore } from "../store/authUser";
 
 const SignUpPage = () => {
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+	const { signup } = useAuthStore();
+
     const handleSignUp = (e) => {
         e.preventDefault();
-        console.log(email, username, password);
+		signup({ email, username, password });
     }
   return (
     <div className="h-screen w-full hero-bg">
